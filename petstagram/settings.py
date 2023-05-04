8656e965978995f7a58a7f3b13ba9c38324974ca
Atanas Stankin <a_stankin@abv.bg>
2023-05-04 22:44:50 +0300

    Update

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%)^jn918l&+j@a7p@sf-=_f%t0apni#5=sns(+6!lc_tvjg+jf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
