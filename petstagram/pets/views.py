71e0da9b68fb5cb52808a5cd348e2fbd77e864fa
Atanas Stankin <a_stankin@abv.bg>
2023-05-05 15:53:57 +0300

    Update

--- petstagram/pets/views.py
+++ petstagram/pets/views.py
@@ -6,13 +6,13 @@ def add_pet(request):
     return render(request, 'pets/pet-add-page.html')
 
 
-def pet_details(request):
+def pet_details(request, username, pet_name):
     return render(request, 'pets/pet-details-page.html')
 
 
-def edit_pet(request):
+def edit_pet(request, username, pet_name):
     return render(request, 'pets/pet-edit-page.html')
 
 
-def delete_pet(request):
+def delete_pet(request, username, pet_name):
     return render(request, 'pets/pet-delete-page.html')
