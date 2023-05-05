d4a6858b92aa6a7c43494c27184151327e8baa44
Atanas Stankin <a_stankin@abv.bg>
2023-05-05 15:49:54 +0300

    Update

--- petstagram/accounts/urls.py
+++ petstagram/accounts/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from petstagram.accounts import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
+    path('login/', views.login_user, name='login'),
+    path('register/', views.register_user, name='register'),
     path('profile/<int:pk>/', include([
         path('', views.show_profile_details, name='profile-details'),
         path('edit/', views.edit_profile, name='profile-edit'),
--- petstagram/accounts/views.py
+++ petstagram/accounts/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def register(request):
+def register_user(request):
     return render(request, 'accounts/register-page.html')
 
 
-def login(request):
+def login_user(request):
     return render(request, 'accounts/login-page.html')
 
 
