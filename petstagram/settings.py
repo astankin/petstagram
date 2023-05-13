5ee114ba54de57cafad4fedb84f987d8fd053877
Atanas Stankin <a_stankin@abv.bg>
2023-05-13 19:40:11 +0300

    Added Pets and Photos Models

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -68,14 +68,16 @@ WSGI_APPLICATION = 'petstagram.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "petstagram_db",
+        "USER": "root",
+        "PASSWORD": "root",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
