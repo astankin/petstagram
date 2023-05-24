3e2305070fd16680144e8f82024d8ea61690d4a9
Atanas Stankin <a_stankin@abv.bg>
2023-05-24 10:15:08 +0300

    Update

--- petstagram/pets/views.py
+++ petstagram/pets/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from petstagram.pets.forms import PetForm, PetDeleteForm
 from petstagram.pets.models import Pet
 
 
 # Create your views here.
 def add_pet(request):
-    return render(request, 'pets/pet-add-page.html')
+    if request.method == 'POST':
+        form = PetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('profile-details', pk=1)
+    else:
+        form = PetForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'pets/pet-add-page.html', context)
 
 
 def pet_details(request, username, pet_slug):
@@ -18,10 +29,30 @@ def pet_details(request, username, pet_slug):
     return render(request, template_name='pets/pet-details-page.html', context=context)
 
 
-def edit_pet(request, username, pet_name):
-    return render(request, 'pets/pet-edit-page.html')
+def edit_pet(request, username, pet_slug):
+    # TODO: user and pet_slug wen auth
+    pet = Pet.objects.get(slug=pet_slug)
+    if request.method == 'GET':
+        form = PetForm(instance=pet)
+    else:
+        form = PetForm(request.POST, instance=pet)
+        if form.is_valid():
+            form.save()
+            return redirect('pet-details', username, pet_slug)
+    context = {
+        'form': form
+    }
+    return render(request, 'pets/pet-edit-page.html', context)
 
 
-def delete_pet(request, username, pet_name):
-    return render(request, 'pets/pet-delete-page.html')
+def delete_pet(request, username, pet_slug):
+    pet = Pet.objects.get(slug=pet_slug)
+    if request.method == 'POST':
+        pet.delete()
+        return redirect('profile-details', pk=1)
+    form = PetDeleteForm(initial=pet.__dict__)
+    context = {
+        'form': form
+    }
+    return render(request, 'pets/pet-delete-page.html', context)
 
